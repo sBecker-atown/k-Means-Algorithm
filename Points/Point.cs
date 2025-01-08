456a011bf53acd077062aef287bb1afeb5dca39e
sBecker-atown <admin@sebastianbeckerart.de>
2025-01-08 10:29:06 +0100

    Added SetCluster method to use in ReclusterPoints method in PointController.

--- Points/Point.cs
+++ Points/Point.cs
@@ -6,7 +6,7 @@ public class Point
 {
     public int X { get; }
     public int Y { get; }
-    public int ClusterId { get; }
+    public int ClusterId { get; private set; }
 
     public Point(int x, int y, int clusterId)
     {
@@ -19,5 +19,9 @@ public class Point
     {
         return Math.Sqrt(Math.Pow(pointB.X - this.X, 2) + Math.Pow(pointB.Y - this.Y, 2));
     }
-    
+
+    public void SetCluster(int clusterId)
+    {
+        ClusterId = clusterId;
+    }
 }
