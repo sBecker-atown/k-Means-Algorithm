942435f0f475dfd62bec0f94542e8ac5b4810473
sBecker-atown <admin@sebastianbeckerart.de>
2025-01-08 10:32:48 +0100

    Using primary constructor.

--- Centroids/Centroid.cs
+++ Centroids/Centroid.cs
@@ -2,9 +2,4 @@ using KMean_Algo.Points;
 
 namespace KMean_Algo.Centroids;
 
-public class Centroid : Point
-{
-    public Centroid(int x, int y, int clusterId) : base(x, y, clusterId)
-    {
-    }
-}
+public class Centroid(int x, int y, int clusterId) : Point(x, y, clusterId);
