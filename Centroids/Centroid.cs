cfd0f91351a1d2c93eeb54502c97f7a217ea8b57
sBecker-atown <admin@sebastianbeckerart.de>
2025-01-13 13:34:19 +0100

    changed to independent class instead of inherited class, so that setters can be used.

--- Centroids/Centroid.cs
+++ Centroids/Centroid.cs
@@ -1,5 +1,24 @@
-using KMean_Algo.Points;
-
 namespace KMean_Algo.Centroids;
 
-public class Centroid(int x, int y, int clusterId) : Point(x, y, clusterId);
+public class Centroid(double x, double y, int clusterId) {
+    
+    public double X { get; private set; } = x;
+    public double Y { get; private set; } = y;
+    public int ClusterId { get; private set; } = clusterId;
+
+    public void SetX(double x)
+    {
+        X = x;
+    }
+
+    public void SetY(double y)
+    {
+        Y = y;
+    }
+
+    override 
+    public string ToString()
+    {
+        return $"({X}, {Y}, ID: {ClusterId})";
+    }
+}
