825e9c4337e77b81da77b1649daf03a390c8e0b4
sBecker-atown <admin@sebastianbeckerart.de>
2025-01-13 13:34:38 +0100

    Finished Recenter method

--- Centroids/CentroidCalculator.cs
+++ Centroids/CentroidCalculator.cs
@@ -7,5 +7,22 @@ internal class CentroidCalculator
     public void RecenterAll(List<Centroid> centroids, List<Point> points)
     {
         // Recenter Centroids based on cluster points.
+        foreach (Centroid centroid in centroids)
+        {
+            double sumX = 0;
+            double sumY = 0;
+            var numPoints = 0;
+            foreach (var point in points)
+            {
+                if (point.ClusterId == centroid.ClusterId)
+                {
+                    sumX += point.X;
+                    sumY += point.Y;
+                    numPoints++;
+                }
+            }
+            centroid.SetX(sumX / numPoints);
+            centroid.SetY(sumY / numPoints);
+        }
     }
 }
