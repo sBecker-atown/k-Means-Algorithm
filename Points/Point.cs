6758855a2fec417d1955619c0af6f95b588e7693
sBecker-atown <admin@sebastianbeckerart.de>
2025-01-15 20:19:40 +0100

    Removed setter, changed order of coordinates in Distance method because results are more reliable and accurate this way. Removed SetCluster Method and used setter instead.

--- Points/Point.cs
+++ Points/Point.cs
@@ -3,18 +3,13 @@ using KMean_Algo.Centroids;
 namespace KMean_Algo.Points;
 
 public class Point(double x, double y, int clusterId) {
-    public double X { get; private set; } = x;
-    public double Y { get; private set; } = y;
-    public int ClusterId { get; private set; } = clusterId;
+    public double X { get; } = x;
+    public double Y { get; } = y;
+    public int ClusterId { get; set; } = clusterId;
 
     public double Distance(Centroid pointB)
     {
-        return Math.Sqrt(Math.Pow(pointB.X - this.X, 2) + Math.Pow(pointB.Y - this.Y, 2));
-    }
-
-    public void SetCluster(int clusterId)
-    {
-        ClusterId = clusterId;
+        return Math.Sqrt(Math.Pow(this.X - pointB.X, 2) + Math.Pow(this.Y - pointB.Y, 2));
     }
     
     override 
