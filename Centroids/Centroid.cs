d9f2f34a3928dbdc756a8f4c6cf13017139f4ed0
sBecker-atown <admin@sebastianbeckerart.de>
2025-01-19 15:16:59 +0100

    removed deprecated private setter on ClusterId

--- Centroids/Centroid.cs
+++ Centroids/Centroid.cs
@@ -4,7 +4,7 @@ public class Centroid(double x, double y, int clusterId) {
     
     public double X { get; private set; } = x;
     public double Y { get; private set; } = y;
-    public int ClusterId { get; private set; } = clusterId;
+    public int ClusterId { get; } = clusterId;
 
     public void SetX(double x)
     {
